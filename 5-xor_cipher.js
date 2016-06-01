c215b30c8980451a3a266a1310b8fff3b4725c98
Roneesh <roneesh@gmail.com>
2016-06-01 19:43:40 -0400

    updating xor cipher code for single key

--- 5-xor_cipher.js
+++ 5-xor_cipher.js
@@ -1,12 +1,11 @@
 function xor_cipher_enc(msg, key) {
 
-	var key = prep_enc_key(msg, key);
-		msg_buffer = new Buffer(msg, 'utf-8'),
+	var msg_buffer = new Buffer(msg, 'utf-8'),
 		key_buffer = new Buffer(key, 'utf-8'),
 		cipher_buffer = new Buffer(msg_buffer.length);
 
 	for (var i = 0; i < msg_buffer.length; i++) {
-		cipher_buffer[i] = (msg_buffer[i] ^ key_buffer[i]);
+		cipher_buffer[i] = msg_buffer[i] ^ key_buffer[0];
 	}
 	
 	return cipher_buffer.toString('hex');
@@ -17,56 +16,22 @@ function xor_cipher_dec(cipher, key) {
 		key_buffer = new Buffer(key, 'utf-8'),
 		msg_buffer = new Buffer(cipher_buffer.length);
 
-	key_buffer = prep_dec_key(cipher_buffer, key_buffer);
-
 	for (var i = 0; i < cipher_buffer.length; i++) {
-		msg_buffer[i] = cipher_buffer[i] ^ key_buffer[i];
+		msg_buffer[i] = cipher_buffer[i] ^ key_buffer[0];
 	}
 
 	return msg_buffer.toString('utf-8');
 }
 
-function prep_enc_key(msg, key) {	
-	
-	if (key.length > msg.length) {
-		key = key.slice(0, msg.length);
-	} 
-	else if (key.length < msg.length) {
-		var orig_key = key;
-			diff = msg.length - key.length,
-			pad = diff % key.length,
-			div = Math.floor(diff / key.length),
-			i = 0,
-			trail = key.slice(0, pad);
-		for (i = 0; i < div; i++) {
-			key += orig_key;
-		}
-		key += trail;
-	}
-	return key;
-}
-function prep_dec_key(msg_buffer, key_buffer) {
-	if (msg_buffer.length < key_buffer.length) {
-		key_buffer = key_buffer.slice(0, msg_buffer.length);
-	} else if (msg_buffer.length > key_buffer.length) {
-		var orig_key = key_buffer,
-			diff = msg_buffer.length - key_buffer.length,
-			division = Math.floor(diff / key_buffer.length),
-			modulo = diff % key_buffer.length;
+var encrypted_message = xor_cipher_enc("Cooking MC\'s like a pound of bacon", 'X');
+console.log('the secret is: ', encrypted_message);
+var decrypted_message = xor_cipher_dec(encrypted_message, 'X');
+console.log('the original message was: ', decrypted_message)
 
-		for (var i = 0; i < division; i++) {
-			key_buffer += orig_key;
-		}
-		key_buffer += orig_key.slice(0,modulo);
-
-		return new Buffer(key_buffer, 'utf-8');
-	}
-}
-
-// var encrypted_message = xor_cipher_enc("Cooking MC\'s like a pound of bacon", 'X');
-// console.log('the secret is: ', encrypted_message);
-// var decrypted_message = xor_cipher_dec(encrypted_message, 'X');
-// console.log('the original message was: ', decrypted_message)
+var em2 = xor_cipher_enc('C', 'X');
+console.log('single secret is: ', em2);
+var dm2 = xor_cipher_dec(em2, 'X');
+console.log('decrypted mesage is: ', dm2);
 
 module.exports = {
 	enc : xor_cipher_enc,
