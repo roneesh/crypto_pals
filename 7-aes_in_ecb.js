0bd078cb71a9ed0aa6290b107df1a230106a5e08
Roneesh <roneesh@gmail.com>
2016-06-10 16:41:52 -0400

    adding in encrypt and making into functions

--- 7-aes_in_ecb.js
+++ 7-aes_in_ecb.js
@@ -1,12 +1,29 @@
 var crypto = require('crypto'),
 	fs = require('fs');
 
-var input = fs.readFileSync('./ciphers/7-cipher.txt', 'utf-8'),
-	inputBuffer = new Buffer(input, 'base64'),
+// encrypt
+var plainTextBuffer = fs.readFileSync('./ciphers/7-plainText.txt', 'utf8'),
 	key = new Buffer('YELLOW SUBMARINE'),
 	iv = '';
 
-var decipher = crypto.createDecipheriv('aes-128-ecb', key, iv);
-var plaintext = Buffer.concat([decipher.update(inputBuffer), decipher.final()])
+function encryptAESecb(textBuffer, key, iv) {
+	var cipher = crypto.createCipheriv('aes-128-ecb', key, iv);
+	return Buffer.concat([cipher.update(textBuffer), cipher.final()]).toString('base64');
+}
 
-console.log(plaintext.toString());
+// var encryptedFromPT = encryptAESecb(plainTextBuffer, key, iv)
+
+// decrypt
+var input = fs.readFileSync('./ciphers/7-cipher.txt', 'utf-8'),
+	inputBuffer = new Buffer(input, 'base64');
+// var inputBuffer = new Buffer(encryptedFromPT, 'base64');
+
+function decryptAESecb(dataBuffer, key, iv) {
+	var decipher = crypto.createDecipheriv('aes-128-ecb', key, iv);
+	return Buffer.concat([decipher.update(dataBuffer), decipher.final()]).toString();
+}
+
+module.exports = {
+	enc : encryptAESecb,
+	dec : decryptAESecb
+}
